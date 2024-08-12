435f12de2dbc9d0a9d67cd231e853a2648e11117
f-akamatsu <fuhito615@gmail.com>
2024-08-12 21:38:36 +0900

    枠

--- app/src/components/features/Field/Field.tsx
+++ app/src/components/features/Field/Field.tsx
@@ -2,6 +2,7 @@ import { Frame } from '../../common/Frame/Frame';
 import { PuyoOnFieldBody } from './PuyoOnFieldBody/PuyoOnFieldBody';
 import { FieldCoordInterface, FieldPuyoInterface } from '@/interfaces/FieldInterfaces';
 import { Score } from './Score/Score';
+import { Flex } from '@chakra-ui/react';
 
 export interface FieldProps {
   fieldPuyos: FieldPuyoInterface[];
@@ -12,10 +13,10 @@ export interface FieldProps {
 export function Field({ fieldPuyos, score, onClickFieldCell }: FieldProps) {
   return (
     <Frame>
-      <>
+      <Flex flexDir='column'>
         <PuyoOnFieldBody onClickFieldCell={onClickFieldCell} fieldPuyos={fieldPuyos} />
         <Score score={score} />
-      </>
+      </Flex>
     </Frame>
   );
 }
