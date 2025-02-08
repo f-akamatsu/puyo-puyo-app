ffd602266344f693497659030efb1c317c23c7c5
f-akamatsu <fuhito615@gmail.com>
2025-02-08 21:05:53 +0900

    graphql sandbox

--- apps/bff/src/common/graphql/custom-graphql.module.ts
+++ apps/bff/src/common/graphql/custom-graphql.module.ts
@@ -2,6 +2,7 @@ import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
 import { join } from 'path';
+import { ApolloServerPluginLandingPageLocalDefault } from '@apollo/server/plugin/landingPage/default';
 
 @Module({
   imports: [
@@ -9,6 +10,8 @@ import { join } from 'path';
       driver: ApolloDriver,
       autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
       sortSchema: true,
+      playground: false,
+      plugins: [ApolloServerPluginLandingPageLocalDefault()],
     }),
   ],
 })
