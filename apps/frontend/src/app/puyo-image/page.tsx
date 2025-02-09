323bebf93c25874ef730aad98ce015288be734d0
f-akamatsu <fuhito615@gmail.com>
2025-02-09 15:26:15 +0900

    graphql

---
+++ apps/frontend/src/app/puyo-image/page.tsx
@@ -0,0 +1,7 @@
+'use client';
+
+import { PuyoImagePresenter } from './presenter';
+
+export default function PuyoImage() {
+  return <PuyoImagePresenter />;
+}
---
+++ apps/frontend/src/app/puyo-image/presenter.tsx
@@ -0,0 +1,5 @@
+export interface PuyoImagePresenterProps {}
+
+export function PuyoImagePresenter({}: PuyoImagePresenterProps) {
+  return <></>;
+}
