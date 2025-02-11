0e9929535011330a109054925a88f5f43375421a
f-akamatsu <fuhito615@gmail.com>
2025-02-11 20:25:58 +0900

    システムのカラーモードを使用しない

--- apps/frontend/src/components/ui/provider.tsx
+++ apps/frontend/src/components/ui/provider.tsx
@@ -1,15 +1,15 @@
-"use client"
+'use client';
 
-import { ChakraProvider, defaultSystem } from "@chakra-ui/react"
-import {
-  ColorModeProvider,
-  type ColorModeProviderProps,
-} from "./color-mode"
+import { ChakraProvider, defaultSystem } from '@chakra-ui/react';
+import { ColorModeProvider, type ColorModeProviderProps } from './color-mode';
+import { ThemeProvider } from 'next-themes';
 
 export function Provider(props: ColorModeProviderProps) {
   return (
-    <ChakraProvider value={defaultSystem}>
-      <ColorModeProvider {...props} />
-    </ChakraProvider>
-  )
+    <ThemeProvider enableSystem={false}>
+      <ChakraProvider value={defaultSystem}>
+        <ColorModeProvider {...props} />
+      </ChakraProvider>
+    </ThemeProvider>
+  );
 }
